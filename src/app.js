3db975f6c0a4fd163001f5e43274d24d78024441
Edward Delaporte <edthedev@gmail.com>
2016-02-15 13:27:25 -0600

    60 or 90 second cool down.
    Press middle button to change cool down length.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 /**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
+ * Lift! by EdTheDev
+ * Copyright (c) 2016 Edward Delaporte
+ * Licensed under the MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 var UI = require('ui');
-// var Vector2 = require('vector2');
 var Wakeup = require('wakeup');
 var Vibe = require('ui/vibe');
 var sets = 0;
@@ -13,7 +15,6 @@ var coolDown = 90;
 
 var main = new UI.Card({
   title: 'Lift!',
-  // icon: 'images/menu_icon.png',
   subtitle: 'by EdTheDev',
   body: 'Press Up to record a set, down to start cooldown timer.',
   subtitleColor: 'indigo', // Named colors
@@ -21,25 +22,22 @@ var main = new UI.Card({
 });
 main.show();
 
-main.on('click', 'up', function(e) {
-  console.log('Main click up!');
-  sets += 1;
-  displaySets();
-});
-
 function displaySets() {
   main.subtitle("Completed: " + sets);
 }
 
+main.on('click', 'up', function(e) {
+  sets += 1;
+  displaySets();
+});
+
 main.on('click', 'select', function(e) {
-  console.log('Main click!');
   if(coolDown == 90) {
     coolDown = 60;
   } else {
     coolDown = 90;
   }
   main.body('Cool down is ' + coolDown + ' seconds.');
-  console.log('Cool down is ' + coolDown + ' seconds.');
 });
 
 main.on('click', 'down', function(e) {
